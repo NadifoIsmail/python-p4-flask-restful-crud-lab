523053214cc6a915e3c58fae293772c5a00d3993
Nadifo Ismail <nadifoismail80@gmail.com>
2025-02-05 08:18:22 +0300

    modifies app.py

--- server/app.py
+++ server/app.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
@@ -16,7 +14,6 @@ db.init_app(app)
 
 api = Api(app)
 
-
 class Plants(Resource):
 
     def get(self):
@@ -24,6 +21,7 @@ class Plants(Resource):
         return make_response(jsonify(plants), 200)
 
     def post(self):
+
         data = request.get_json()
 
         new_plant = Plant(
@@ -37,19 +35,41 @@ class Plants(Resource):
 
         return make_response(new_plant.to_dict(), 201)
 
-
 api.add_resource(Plants, '/plants')
 
-
 class PlantByID(Resource):
-
     def get(self, id):
-        plant = Plant.query.filter_by(id=id).first().to_dict()
-        return make_response(jsonify(plant), 200)
+        plant = Plant.query.filter_by(id=id).first()
+        if plant:
+            return make_response(jsonify(plant.to_dict()), 200)
+        return make_response(jsonify({"error": "Plant not found"}), 404)
 
+    def patch(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        if not plant:
+            return make_response(jsonify({"error": "Plant not found"}), 404)
 
+        data = request.get_json()
+        if "is_in_stock" in data:
+            plant.is_in_stock = data["is_in_stock"]
+
+        db.session.commit()
+        return make_response(jsonify(plant.to_dict()), 200)
+    
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        if not plant:
+            return make_response('', 404)  
+
+        db.session.delete(plant)
+        db.session.commit()
+
+        return make_response('', 204)
+    
+    
+    
 api.add_resource(PlantByID, '/plants/<int:id>')
-
+        
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
